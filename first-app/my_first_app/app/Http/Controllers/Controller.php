690f031f04c0121a5dec7378ac464511e3723260
bandaronda <131749924+bandaronda@users.noreply.github.com>
2023-11-02 23:52:49 +0100

    Update Controller.php

--- first-app/my_first_app/app/Http/Controllers/Controller.php
+++ first-app/my_first_app/app/Http/Controllers/Controller.php
@@ -8,5 +8,27 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    private $planets = [
+        'Mercury',
+        'Venus',
+        'Earth',
+        'Mars',
+        'Jupiter',
+        'Saturn',
+        'Uranus',
+        'Neptune'
+    ];
+    public function index() {
+        return view('planets.index', ['planets' => $this->planets]);
+    }
+    
+    public function show($planet) {
+        if (in_array($planet, $this->planets)) {
+            return view('planets.show', ['planet' => $planet]);
+        } else {
+            return redirect('/planets');
+        }
+    }
+    
     use AuthorizesRequests, ValidatesRequests;
 }
