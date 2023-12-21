4496e2040d35cd9b0dc98f977a60108b09e583c5
bandaronda <131749924+bandaronda@users.noreply.github.com>
2023-12-21 12:11:32 +0100

    Update planet_controller.php

--- first-app/my_first_app/app/Http/Controllers/planet_controller.php
+++ first-app/my_first_app/app/Http/Controllers/planet_controller.php
@@ -1,35 +1,25 @@
 <?php
-
 namespace App\Http\Controllers;
 
+use App\Models\Planet;
 use Illuminate\Http\Request;
 
 class PlanetController extends Controller
 {
-    private $planets = [
-        [
-            'name' => 'Mars',
-            'description' => 'Mars is the fourth planet from the Sun...'
-        ],
-        [
-            'name' => 'Venus',
-            'description' => 'Venus is the second planet from the Sun...'
-        ],
-        // ... (Voeg de andere planeten hier toe)
-    ];
-
-    public function index() {
-        return view('planets', ['planets' => $this->planets]);
+ 
+    public function index()
+    {
+        $planets = Planet::all();
+        return view('planets.index', ['planets' => $planets]);
     }
-
-    public function show($planetName = null) {
-        $planetsCollection = collect($this->planets);
-
-        if ($planetName) {
-            $planetsCollection = $planetsCollection->filter(function ($planet) use ($planetName) {
-                return strtolower($planet['name']) === strtolower($planetName);
-            });
-        }
+    
+    public function show($planetName)
+    {
+        $planet = Planet::where('name', $planetName)->first();
+        return view('planets.show', ['planet' => $planet]);
+    }
+    
+}
 
         return view('planets', ['planets' => $planetsCollection->all()]);
     }
